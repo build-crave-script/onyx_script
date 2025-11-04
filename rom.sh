d7305aecd0dab1d11353f8c75d089ef22e31dbe3
K4LCHAKRA <oseverest44@gmail.com>
2025-11-04 09:53:53 +0545

    Add files via upload

---
+++ rom.sh
@@ -0,0 +1,22 @@
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+# /opt/crave/resync.sh
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+
+rm -rf device/xiaomi/onyx
+rm -rf vendor/xiaomi/onyx
+rm -rf device/xiaomi/onyx-kernel
+rm -rf hardware/xiaomi
+rm -rf packages/apps/XiaomiDolby
+
+git clone https://github.com/poco-f7-onyx/android_device_xiaomi_onyx.git -b lineage-23.0 device/xiaomi/onyx
+git clone https://github.com/poco-f7-onyx/proprietary_vendor_xiaomi_onyx.git -b lineage-23.0 vendor/xiaomi/onyx
+git clone https://github.com/poco-f7-onyx/android_device_xiaomi_onyx-kernel.git -b lineage-23.0 device/xiaomi/onyx-kernel
+git clone https://github.com/poco-f7-onyx/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
+git clone https://github.com/poco-f7-onyx/android_packages_apps_XiaomiDolby.git -b lineage-22.2 packages/apps/XiaomiDolby
+git clone https://github.com/poco-f7-onyx/vendor_infinity-priv_keys.git -b main vendor/infinity-priv/keys
+
+. build/envsetup.sh
+export TARGET_SUPPORTS_BLUR=true
+export WITH_GAPPS=true
+lunch infinity_onyx-userdebug
+m bacon
