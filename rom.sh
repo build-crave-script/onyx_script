96528e519cefe1ecc19644b740004561fc3cb30c
K4LCHAKRA <oseverest44@gmail.com>
2025-11-08 02:27:48 +0545

    Revise build setup in rom.sh
    Updated build environment setup and configurations.

--- rom.sh
+++ rom.sh
@@ -16,8 +16,16 @@ git clone https://github.com/poco-f7-onyx/android_hardware_xiaomi.git -b lineage
 git clone https://github.com/poco-f7-onyx/android_packages_apps_XiaomiDolby.git -b lineage-22.2 packages/apps/XiaomiDolby
 git clone https://github.com/poco-f7-onyx/vendor_infinity-priv_keys.git -b main vendor/infinity-priv/keys
 
-. build/envsetup.sh
-export TARGET_SUPPORTS_BLUR=true
-export WITH_GAPPS=true
-lunch infinity_onyx-userdebug
-m bacon
+source build/make/envsetup.sh
+source build/envsetup.sh
+source build/make/envsetup.sh
+export WITH_GMS=true
+export TARGET_USES_MINI_GAPPS=true
+export TARGET_ENABLE_BLUR=true
+export TARGET_PREBUILT_LAWNCHAIR_LAUNCHER=true
+export TARGET_DEFAULT_PIXEL_LAUNCHER=false
+export PERSIST_SYS_DEFAULT_LAUNCHER=0  
+export PERSIST_SYS_QUICKSWITCH_PIXEL_SHIPPED=1
+riseup sunny user
+make installclean
+rise b
