9ba248f2536adcb4891c92a3089e53f8a67f5892
K4LCHAKRA <oseverest44@gmail.com>
2025-11-08 02:25:17 +0545

    Update repository initialization and cloning commands

--- rom.sh
+++ rom.sh
@@ -1,6 +1,7 @@
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
-# /opt/crave/resync.sh
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo init -u https://github.com/RisingOS-Revived/android -b sixteen --git-lfs
+# repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+/opt/crave/resync.sh
+# repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 rm -rf device/xiaomi/onyx
 # rm -rf vendor/xiaomi/onyx
@@ -8,7 +9,7 @@ rm -rf device/xiaomi/onyx
 # rm -rf hardware/xiaomi
 # rm -rf packages/apps/XiaomiDolby
 
-git clone https://github.com/poco-f7-onyx/android_device_xiaomi_onyx.git -b lineage-23.0 device/xiaomi/onyx
+git clone https://github.com/poco-f7-onyx/android_device_xiaomi_onyx.git -b ros device/xiaomi/onyx
 git clone https://github.com/poco-f7-onyx/proprietary_vendor_xiaomi_onyx.git -b lineage-23.0 vendor/xiaomi/onyx
 git clone https://github.com/poco-f7-onyx/android_device_xiaomi_onyx-kernel.git -b lineage-23.0 device/xiaomi/onyx-kernel
 git clone https://github.com/poco-f7-onyx/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
