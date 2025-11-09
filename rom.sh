d7b0f36d8c44ebc06dba43b6897f509feb6ff495
K4LCHAKRA <oseverest44@gmail.com>
2025-11-09 10:04:41 +0545

    Update rom.sh

--- rom.sh
+++ rom.sh
@@ -15,7 +15,7 @@ git clone https://github.com/poco-f7-onyx/proprietary_vendor_xiaomi_onyx.git -b 
 git clone https://github.com/poco-f7-onyx/android_device_xiaomi_onyx-kernel.git -b lineage-23.0 device/xiaomi/onyx-kernel
 git clone https://github.com/poco-f7-onyx/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
 git clone https://github.com/poco-f7-onyx/android_packages_apps_XiaomiDolby.git -b lineage-22.2 packages/apps/XiaomiDolby
-git clone https://github.com/poco-f7-onyx/vendor_infinity-priv_keys.git -b main vendor/lineage-priv/keys
+# git clone https://github.com/poco-f7-onyx/vendor_infinity-priv_keys.git -b main vendor/lineage-priv/keys
 rm -rf official/devices
 git clone https://github.com/K4LCHAKRA/official_devices_rising.git -b fifteen official/devices
 
