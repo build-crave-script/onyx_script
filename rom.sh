2b99bbee630df7b147cd45b55667380d16f4a2b6
Rom Bdr. Gharti <romgharti@gmail.com>
2025-11-15 08:08:48 +0545

    onyx: Neoteric-OS

--- rom.sh
+++ rom.sh
@@ -1,14 +1,14 @@
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/romgharti/stable_releases.git -b waterlily -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/Neoteric-OS/android_manifest -g all -b bka,-mips,-darwin,-notdefault
 /opt/crave/resync.sh
 # repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 rm -rf device/xiaomi/onyx
-# rm -rf vendor/xiaomi/onyx
-# rm -rf device/xiaomi/onyx-kernel
-# rm -rf hardware/xiaomi
-# rm -rf packages/apps/XiaomiDolby
+rm -rf vendor/xiaomi/onyx
+rm -rf device/xiaomi/onyx-kernel
+rm -rf hardware/xiaomi
+#rm -rf packages/apps/XiaomiDolby
 
-git clone https://github.com/poco-f7-onyx/android_device_xiaomi_onyx.git -b bliss device/xiaomi/onyx
+git clone https://github.com/poco-f7-onyx/android_device_xiaomi_onyx.git -b ne device/xiaomi/onyx
 git clone https://github.com/poco-f7-onyx/proprietary_vendor_xiaomi_onyx.git -b lineage-23.0 vendor/xiaomi/onyx
 git clone https://github.com/poco-f7-onyx/android_device_xiaomi_onyx-kernel.git -b lineage-23.0 device/xiaomi/onyx-kernel
 git clone https://github.com/poco-f7-onyx/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
@@ -16,4 +16,4 @@ git clone https://github.com/poco-f7-onyx/android_packages_apps_XiaomiDolby.git 
 git clone https://gitea.com/goziko32/vendor_gcam.git vendor/mgc
 
 . build/envsetup.sh
-blissify -c -d onyx
+m bacon
