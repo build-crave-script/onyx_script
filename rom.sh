683c0c32acc207efaae6b66b772eedd162c944fd
Rom Bdr. Gharti <romgharti@gmail.com>
2025-11-15 11:31:14 +0545

    Update rom.sh

--- rom.sh
+++ rom.sh
@@ -16,4 +16,5 @@ git clone https://github.com/poco-f7-onyx/android_packages_apps_XiaomiDolby.git 
 git clone https://gitea.com/goziko32/vendor_gcam.git vendor/mgc
 
 . build/envsetup.sh
+lunch onyx-userdebug
 m bacon
